9544b6f54b100f5357a4d773cb4ababcfdd919f2
dansarpong <dansarpong.me@gmail.com>
2025-05-28 10:53:39 +0000

    fix: Refactor RDS module outputs to handle cases with no read replicas

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,27 +1,27 @@
 # DB Instance Outputs
 output "db_instance_address" {
   description = "The address of the RDS instance"
-  value       = local.is_primary ? aws_db_instance.this[0].address : aws_db_instance.replica[0].address
+  value       = local.is_primary ? aws_db_instance.this[0].address : (length(aws_db_instance.replica) > 0 ? aws_db_instance.replica[0].address : null)
 }
 
 output "db_instance_arn" {
   description = "The ARN of the RDS instance"
-  value       = local.is_primary ? aws_db_instance.this[0].arn : aws_db_instance.replica[0].arn
+  value       = local.is_primary ? aws_db_instance.this[0].arn : (length(aws_db_instance.replica) > 0 ? aws_db_instance.replica[0].arn : null)
 }
 
 output "db_instance_endpoint" {
   description = "The connection endpoint of the RDS instance"
-  value       = local.is_primary ? aws_db_instance.this[0].endpoint : aws_db_instance.replica[0].endpoint
+  value       = local.is_primary ? aws_db_instance.this[0].endpoint : (length(aws_db_instance.replica) > 0 ? aws_db_instance.replica[0].endpoint : null)
 }
 
 output "db_instance_id" {
   description = "The RDS instance ID"
-  value       = local.is_primary ? aws_db_instance.this[0].id : aws_db_instance.replica[0].id
+  value       = local.is_primary ? aws_db_instance.this[0].id : (length(aws_db_instance.replica) > 0 ? aws_db_instance.replica[0].id : null)
 }
 
 output "db_instance_identifier" {
   description = "The RDS instance identifier"
-  value       = local.is_primary ? aws_db_instance.this[0].identifier : aws_db_instance.replica[0].identifier
+  value       = local.is_primary ? aws_db_instance.this[0].identifier : (length(aws_db_instance.replica) > 0 ? aws_db_instance.replica[0].identifier : null)
 }
 
 output "db_instance_name" {
@@ -37,12 +37,12 @@ output "db_instance_username" {
 
 output "db_instance_port" {
   description = "The database port"
-  value       = local.is_primary ? aws_db_instance.this[0].port : aws_db_instance.replica[0].port
+  value       = local.is_primary ? aws_db_instance.this[0].port : (length(aws_db_instance.replica) > 0 ? aws_db_instance.replica[0].port : null)
 }
 
 output "db_instance_ca_cert_identifier" {
   description = "Specifies the identifier of the CA certificate for the DB instance"
-  value       = local.is_primary ? aws_db_instance.this[0].ca_cert_identifier : aws_db_instance.replica[0].ca_cert_identifier
+  value       = local.is_primary ? aws_db_instance.this[0].ca_cert_identifier : (length(aws_db_instance.replica) > 0 ? aws_db_instance.replica[0].ca_cert_identifier : null)
 }
 
 output "db_instance_master_user_secret_arn" {
